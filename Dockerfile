6df695112648f5478c16651cbf7af6032e984471
guqzhou <guqzhou@163.com>
2023-10-05 13:40:15 +0800

    修改bug

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM xhofe/alist:v2.6.4
-LABEL MAINTAINER="i@nn.ci"
-WORKDIR /opt/alist/
-
-EXPOSE 5244
-
-ENTRYPOINT [ "./alist", "-docker" ]
---
+++ assets/pages-cart-cart.8627acf9.js
@@ -0,0 +1,1 @@
+import{d as a,o as s,c as t,w as e,b as r,h as o}from"./index-b300b68c.js";import{_ as c}from"./_plugin-vue_export-helper.1b428a4d.js";const p=c(a({__name:"cart",setup:a=>(a,c)=>{const p=o;return s(),t(p,{class:"cart"},{default:e((()=>[r("cart")])),_:1})}}),[["__scopeId","data-v-293f42f2"]]);export{p as default};
